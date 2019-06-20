13f3b93b10401435f55c50a5e1a8d962533f14ac
Oscar Baracos <oscar.baracos@numberly.com>
2019-06-20 10:33:43 +0200

    Add script for git aliases

---
+++ git_aliases.sh
@@ -0,0 +1,6 @@
+git config --global alias.st status
+git config --global alias.cm 'commit -m'
+git config --global alias.psup 'push --set-upstream origin'
+git config --global alias.cb 'checkout -b'
+git config --global alias.pom 'push origin master'
+git config --global alias.co  checkout
