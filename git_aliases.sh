f49119c047d74753ecb8f6d11647ca4540807ddc
Oscar Baracos <oscar.baracos@numberly.com>
2019-07-03 17:46:53 +0200

    Replaced git aliases with gitconfig

--- git_aliases.sh
+++
@@ -1,6 +0,0 @@
-git config --global alias.st status
-git config --global alias.cm 'commit -m'
-git config --global alias.psup 'push --set-upstream origin'
-git config --global alias.cb 'checkout -b'
-git config --global alias.pom 'push origin master'
-git config --global alias.co  checkout
